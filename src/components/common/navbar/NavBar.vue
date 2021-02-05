5cfc1bbacf490de4a7832b2784b669c3d75412c9
Nicklaus6 <liuminjie_1108@163.com>
2021-02-05 16:11:15 +0800

    feat: 顶部导航栏完成

---
+++ src/components/common/navbar/NavBar.vue
@@ -0,0 +1,47 @@
+<!--
+ * @Author: your name
+ * @Date: 2021-02-05 12:23:24
+ * @LastEditTime: 2021-02-05 14:30:34
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \re-mall-vue\src\components\common\navbar\NavBar.vue
+-->
+<template>
+  <div class="nav-bar space-average" :style="colorStyle">
+    <div class="nav__left"><slot name="left"></slot></div>
+    <div class="nav__middle"><slot name="middle"></slot></div>
+    <div class="nav__right"><slot name="right"></slot></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NavBar",
+  props: {
+    backgroundColor: {
+      type: String,
+      // default: "$color-tint" 不能用 scss变量
+      default: "#ff8198"
+    },
+    fontColor: {
+      type: String,
+      default: "#fff"
+    }
+  },
+  computed: {
+    colorStyle() {
+      return { backgroundColor: this.backgroundColor, color: this.fontColor };
+    }
+  }
+};
+</script>
+
+<style lang="scss" scope>
+.nav {
+  &-bar {
+    height: 44px;
+    line-height: 44px;
+    text-align: center;
+  }
+}
+</style>
--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -1,13 +1,17 @@
 <!--
  * @Author: your name
  * @Date: 2021-02-02 17:36:33
- * @LastEditTime: 2021-02-04 23:52:12
+ * @LastEditTime: 2021-02-05 15:49:13
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \re-mall-vue\src\views\Home.vue
 -->
 <template>
-  <div class="home">
+  <div class="home__container">
+    <nav-bar class="home__nav">
+      <div slot="middle">购物街</div>
+    </nav-bar>
+
     <h1>home</h1>
   </div>
 </template>
@@ -17,3 +21,8 @@ export default {
   name: "Home"
 };
 </script>
+<style lang="scss" scope>
+.home {
+  height: 2000px;
+}
+</style>
