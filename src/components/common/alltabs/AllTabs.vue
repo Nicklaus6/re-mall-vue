c4782bad9a7e91cb7c3fb273058b28d23dfde267
Nicklaus6 <liuminjie_1108@163.com>
2021-02-07 14:56:00 +0800

    feat: 页面滚动高度不够

---
+++ src/components/common/alltabs/AllTabs.vue
@@ -0,0 +1,38 @@
+<!--
+ * @Author: your name
+ * @Date: 2021-02-07 11:44:02
+ * @LastEditTime: 2021-02-07 14:07:40
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \re-mall-vue\src\views\Home\childComps\HomeTabs.vue
+-->
+<template>
+  <tabs class="all__tabs" height="40px" color="red">
+    <tab :title="item" class="tab1" v-for="item in titles" :key="item">
+      <!-- <div v-for="item in 10" :key="item">123</div> -->
+      <slot />
+    </tab>
+  </tabs>
+</template>
+
+<script>
+import { tabs, tab } from "vant";
+export default {
+  name: "AllTabs",
+  components: {
+    tabs,
+    tab
+  },
+  props: {
+    titles: {
+      type: Array,
+      default: () => {}
+    },
+    content: {
+      type: Array
+    }
+  }
+};
+</script>
+
+<style></style>
--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: your name
  * @Date: 2021-02-02 17:36:33
- * @LastEditTime: 2021-02-07 11:05:31
+ * @LastEditTime: 2021-02-07 14:51:51
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \re-mall-vue\src\views\Home.vue
@@ -13,25 +13,19 @@
     </nav-bar>
     <main-swiper class="home__swiper" :banners="banners"></main-swiper>
     <home-recommend></home-recommend>
-    <tabs class="home__tabs" height="40px" color="#666">
-      <tab title="流行" class="tab1">内容1</tab>
-      <tab title="新款" class="tab2">内容2</tab>
-      <tab title="精选" class="tab3">内容3</tab>
-    </tabs>
-    <h1 v-for="item in 80" :key="item">home</h1>
+    <all-tabs :titles="titles" :content="content">
+      <div class="div" v-for="(item, index) in 20" :key="item">{{ index }}</div>
+    </all-tabs>
   </div>
 </template>
 
 <script>
 import HomeRecommend from "./childComps/HomeRecommend";
-import { tabs, tab } from "vant";
 
 export default {
   name: "Home",
   components: {
-    HomeRecommend,
-    tabs,
-    tab
+    HomeRecommend
   },
 
   data() {
@@ -41,13 +35,19 @@ export default {
         "@/assets/img/banner.png",
         "@/assets/img/banner.png",
         "@/assets/img/banner.png"
-      ]
+      ],
+      titles: ["流行", "新款", "精选"],
+      content: ["流行", "新款", "精选"]
     };
   }
 };
 </script>
 <style lang="scss" scope>
 .home {
+  &__container {
+    height: calc(100vh - 49px);
+  }
+
   &__swiper {
     background-color: pink;
     height: 180px;
